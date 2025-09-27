d031de7535424d9dcf2818b996e9aa56581a0825
Vadym <vadym.nitsenko@magnise.com.ua>
2025-09-27 21:26:11 +0200

    Lab1 Vadym Nitsenko

---
+++ vadym-nitsenko-lab1/src/App.vue
@@ -0,0 +1,66 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <div class="greetings">
+      <h1>Name: Nitsenko Vadym</h1>
+      <h1>Group: 6.1214-ipz-1</h1>
+      <h1>Course 2</h1>
+      <h1>Specialization: Software Engineering</h1>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+main {
+  /* make main take available viewport and center its content */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(100vh - 200px);
+  padding: 1rem;
+}
+
+.greetings {
+  text-align: center;
+  line-height: 1.6;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
