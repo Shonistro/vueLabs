d97dfe2b19da3199c8c7fcdbc224ffa9b771a88c
Vadym <vadym.nitsenko@magnise.com.ua>
2025-10-04 22:10:25 +0200

    lab3

---
+++ vadym-nitsenko-lab3/src/components/Tabs/Tabs.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, provide, watch } from 'vue'
+
+const props = defineProps({
+  defaultTab: { type: [String, Number], default: null },
+  variant: { type: String, default: 'underline' } // underline | pills | boxed
+})
+
+const activeTab = ref(props.defaultTab)
+
+function setActive(id) {
+  activeTab.value = id
+}
+
+watch(() => props.defaultTab, v => { activeTab.value = v })
+
+// Provide a simple context for Tab and TabPanel
+provide('tabs', {
+  activeTab,
+  setActive,
+  variant: props.variant
+})
+</script>
+
+<template>
+  <div>
+    <slot name="tabs"></slot>
+    <slot name="panels"></slot>
+  </div>
+</template>
+
+<style scoped>
+</style>
